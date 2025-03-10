5664a656833980da6b72894e8922142332edf4d9
Siddhant Chhikara <153754448+siddhant-chhikara@users.noreply.github.com>
2025-03-10 11:07:43 +0530

    Update Phonebook.c

--- Phonebook.c
+++ Phonebook.c
@@ -11,11 +11,11 @@ int main (void)
 {
     people person[3];
     strcpy(person[0].name, "sid");
-    strcpy(person[0].number, "8800670130");
-    strcpy(person[1].name, "arjun");
-    strcpy(person[1].number, "9911146502");
+    strcpy(person[0].number, "1234567890");
+    strcpy(person[1].name, "papa");
+    strcpy(person[1].number, "0987654321");
     strcpy(person[2].name, "mom");
-    strcpy(person[2].number, "8920103269");
+    strcpy(person[2].number, "9876543210");
     char search[50];
     printf("Enter name : ");
     scanf("%s",search);
